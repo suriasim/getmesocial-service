bd8d146a9675d9b4fd195a27dad9191aa3c0fc83
asimsuri <asimsuri@gmail.com>
2021-08-19 00:21:08 -0400

    first commit

---
+++ src/test/java/com/example/getmesocial/GetmesocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getmesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmesocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
