93e5096aabea6e0d30585147ebb59371acf88b91
asimsuri <asimsuri@gmail.com>
2021-08-19 01:45:12 -0400

    Final version

--- src/main/java/com/example/getmesocial/GetmesocialApplication.java
+++ src/main/java/com/example/getmesocial/GetmesocialApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GetmesocialApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GetmesocialApplication.class, args);
 	}
 
